522b67e0dc30f774f3b7d8649931eaf4b2ef12cb
Richard Huang <flyerhzm@gmail.com>
2019-05-25 18:18:51 +0800

    initial commit

---
+++ lib/rubocop/config/prettier.rb
@@ -0,0 +1,10 @@
+require "rubocop/config/prettier/version"
+
+module Rubocop
+  module Config
+    module Prettier
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rubocop/config/prettier/version.rb
@@ -0,0 +1,7 @@
+module Rubocop
+  module Config
+    module Prettier
+      VERSION = "0.1.0"
+    end
+  end
+end
